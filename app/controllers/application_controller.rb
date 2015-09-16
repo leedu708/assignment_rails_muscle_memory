2035894893f0b93127910b1e689043b0aaf63cd9
leedu708 <djl265@cornell.edu>
2015-09-16 13:55:54 -0400

    added crate method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,29 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def new
+
+    @article = Article.new
+
+  end
+
+  def create
+
+    @article = Article.new(new_article_params)
+    
+    if @article.save
+      redirect_to article_path(@article.id)
+    else
+      render :new
+    end
+
+  end
+
+  def new_article_params
+
+    params.require(:article).permit(:title,:body)
+
+  end
+
 end
