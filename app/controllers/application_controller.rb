af650bbcb54c7b79d76a0b5d690053b2ccb452b0
leedu708 <djl265@cornell.edu>
2015-09-16 14:16:52 -0400

    added index action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,40 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def new
-
-    @article = Article.new
-
-  end
-
-  def create
-
-    @article = Article.new(new_article_params)
-    
-    if @article.save
-      redirect_to article_path(@article.id)
-    else
-      render :new
-    end
-
-  end
-
-  def index
-
-    @articles = Article.all
-
-  end
-
-  def show
-
-    @article = Article.find(params[:id])
-
-  end
-
-  def new_article_params
-
-    params.require(:article).permit(:title,:body)
-
-  end
+  
 
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,39 @@
 class ArticlesController < ApplicationController
+
+  def new
+
+    @article = Article.new
+
+  end
+
+  def create
+
+    @article = Article.new(new_article_params)
+    
+    if @article.save
+      redirect_to article_path(@article.id)
+    else
+      render :new
+    end
+
+  end
+
+  def index
+
+    @articles = Article.all
+
+  end
+
+  def show
+
+    @article = Article.find(params[:id])
+
+  end
+
+  def new_article_params
+
+    params.require(:article).permit(:title,:body)
+
+  end
+  
 end
